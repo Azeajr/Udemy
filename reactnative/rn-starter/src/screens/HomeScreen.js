ae7d7f7967a0ea7af28fd95cc6d3dc10ccf30406
Antonio Zea Jr <halosprtn117@gmail.com>
2021-06-28 01:53:15 -0400

    Section 4 Navigating users between screens

--- reactnative/rn-starter/src/screens/HomeScreen.js
+++ reactnative/rn-starter/src/screens/HomeScreen.js
@@ -1,14 +1,26 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>Hello Antonio;)</Text>;
+const HomeScreen = props => {
+  return (
+    <View>
+      <Text style={styles.text}>Hello Antonio;)</Text>
+      <Button
+        title="Go to Components Demo"
+        onPress={() => props.navigation.navigate('Components')}
+      />
+      <Button
+        title="Go to List Demo"
+        onPress={() => props.navigation.navigate('List')}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   text: {
-    fontSize: 30
-  }
+    fontSize: 30,
+  },
 });
 
 export default HomeScreen;

002f599c86a90411b9b05f201c81f62185feb483
Antonio Zea Jr <halosprtn117@gmail.com>
2021-06-28 23:24:32 -0400

    section 5 building reusable components

---
+++ reactnative/rn-starter/src/components/ImageDetail.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
+
+const ImageDetail = ({ imageSource, title, imageScore }) => {
+  return (
+    <View>
+      <Image source={imageSource} />
+      <Text>{title}</Text>
+      <Text>Image score - {imageScore}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default ImageDetail;
--- reactnative/rn-starter/src/screens/HomeScreen.js
+++ reactnative/rn-starter/src/screens/HomeScreen.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = ({navigation}) => {
+const HomeScreen = ({ navigation }) => {
   return (
     <View>
       <Text style={styles.text}>Hello Antonio;)</Text>
@@ -9,9 +9,15 @@ const HomeScreen = ({navigation}) => {
         title="Go to Components Demo"
         onPress={() => navigation.navigate("Components")}
       />
+
       <Button
         title="Go to List Demo"
         onPress={() => navigation.navigate("List")}
+      />
+
+      <Button
+        title="Go to Image Screen"
+        onPress={() => navigation.navigate("Image")}
       />
     </View>
   );

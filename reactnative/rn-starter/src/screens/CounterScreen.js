624d8106c8c7310c62536a0837a3bcf33c931441
Antonio Zea Jr <halosprtn117@gmail.com>
2021-07-10 01:11:05 -0400

    Using reducer in counterscreen.

--- reactnative/rn-starter/src/screens/CounterScreen.js
+++ reactnative/rn-starter/src/screens/CounterScreen.js
@@ -1,16 +1,31 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 
+const reducer = (state, action) => {
+    //return { ...state, counter: state.counter + action.payload };
+    
+    switch(action.type){
+        case "increase_counter":
+            return { ...state, counter: state.counter + action.payload };
+        case "decrease_counter":
+            return { ...state, counter: state.counter - action.payload };
+        default:
+            return state;
+    }
+}
+
+
 const CounterScreen = () => {
-    const [counter, setCounter] = useState(0);
+    const [state, dispatch] = useReducer(reducer, {counter: 0});
+    const {counter} = state;
 
     return <View>
-        <Button title="Increase"onPress={() => {
-            setCounter(counter + 1);
-        }} />
-        <Button title="Decrease"onPress={() => {
-            setCounter(counter - 1);
-        }} />
+        <Button title="Increase"onPress={() =>
+            dispatch({type: "increase_counter", payload: 1})
+        } />
+        <Button title="Decrease"onPress={() =>
+            dispatch({type: "decrease_counter", payload: 1})
+        } />
         <Text>Current Count: {counter}</Text>
     </View>
 };

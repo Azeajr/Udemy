195900b32ad23303b6f9b6a73ea838294919cf99
Antonio Zea Jr <halosprtn117@gmail.com>
2021-07-12 12:08:52 -0400

    Box object model

---
+++ reactnative/rn-starter/src/screens/BoxScreen.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+const BoxScreen = () => {
+  return (
+    <View style={styles.viewStyle}>
+      <Text style={styles.textStyle}>Child #1</Text>
+      <Text style={styles.textStyle}>Child #2</Text>
+      <Text style={styles.textStyle}>Child #3</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+    viewStyle: {
+        borderWidth:3,
+        borderColor:"black"
+    },
+    textStyle: {
+        borderWidth:10,
+        borderColor: "red",
+        margin: 20
+    }
+});
+
+export default BoxScreen;
--- reactnative/rn-starter/src/screens/HomeScreen.js
+++ reactnative/rn-starter/src/screens/HomeScreen.js
@@ -39,6 +39,11 @@ const HomeScreen = ({ navigation }) => {
         title="Go to Text Demo"
         onPress={() => navigation.navigate("Text")}
       />
+
+      <Button
+        title="Go to Box Demo"
+        onPress={() => navigation.navigate("Box")}
+      />
     </View>
   );
 };

f3b09a46c00f191ef7a50efc936af682de0dff10
Antonio Zea Jr <halosprtn117@gmail.com>
2021-07-12 23:04:50 -0400

    Flex with with parent objects align, flex, and justify

--- reactnative/rn-starter/src/screens/BoxScreen.js
+++ reactnative/rn-starter/src/screens/BoxScreen.js
@@ -4,23 +4,34 @@ import { Text, View, StyleSheet } from "react-native";
 const BoxScreen = () => {
   return (
     <View style={styles.viewStyle}>
-      <Text style={styles.textStyle}>Child #1</Text>
-      <Text style={styles.textStyle}>Child #2</Text>
-      <Text style={styles.textStyle}>Child #3</Text>
+      <Text style={styles.textOneStyle}>Child #1</Text>
+      <Text style={styles.textTwoStyle}>Child #2</Text>
+      <Text style={styles.textThreeStyle}>Child #3</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-    viewStyle: {
-        borderWidth:3,
-        borderColor:"black"
-    },
-    textStyle: {
-        borderWidth:10,
-        borderColor: "red",
-        margin: 20
-    }
+  viewStyle: {
+    borderWidth: 3,
+    borderColor: "black",
+    height: 200,
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "flex-start",
+  },
+  textOneStyle: {
+    borderWidth: 3,
+    borderColor: "red",
+  },
+  textTwoStyle: {
+    borderWidth: 3,
+    borderColor: "red",
+  },
+  textThreeStyle: {
+    borderWidth: 3,
+    borderColor: "red",
+  },
 });
 
 export default BoxScreen;

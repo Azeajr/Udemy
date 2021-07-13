2eed95da4cd5e6191364113e15b14b457761f2a8
Antonio Zea Jr <halosprtn117@gmail.com>
2021-07-12 23:28:22 -0400

    Absolute fill objects

--- reactnative/rn-starter/src/screens/BoxScreen.js
+++ reactnative/rn-starter/src/screens/BoxScreen.js
@@ -16,7 +16,6 @@ const styles = StyleSheet.create({
     borderWidth: 3,
     borderColor: "black",
     height: 200,
-    alignItems: "center"
   },
   textOneStyle: {
     borderWidth: 3,
@@ -25,7 +24,8 @@ const styles = StyleSheet.create({
   textTwoStyle: {
     borderWidth: 3,
     borderColor: "red",
-    alignSelf: "flex-end"
+    fontSize: 18,
+    ...StyleSheet.absoluteFillObject
   },
   textThreeStyle: {
     borderWidth: 3,

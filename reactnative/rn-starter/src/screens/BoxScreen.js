f41e6ad785b5e24b29739c8c9a746ff0b4b9c745
Antonio Zea Jr <halosprtn117@gmail.com>
2021-07-14 12:30:40 -0400

    Screen layout basics

--- reactnative/rn-starter/src/screens/BoxScreen.js
+++ reactnative/rn-starter/src/screens/BoxScreen.js
@@ -4,9 +4,9 @@ import { Text, View, StyleSheet } from "react-native";
 const BoxScreen = () => {
   return (
     <View style={styles.viewStyle}>
-      <Text style={styles.textOneStyle}>Child #1</Text>
-      <Text style={styles.textTwoStyle}>Child #2</Text>
-      <Text style={styles.textThreeStyle}>Child #3</Text>
+      <View style={styles.viewOneStyle} />
+      <View style={styles.viewTwoStyle} />
+      <View style={styles.viewThreeStyle} />
     </View>
   );
 };
@@ -16,20 +16,30 @@ const styles = StyleSheet.create({
     borderWidth: 3,
     borderColor: "black",
     height: 200,
+    flexDirection: "row",
+    justifyContent: "space-between"
   },
-  textOneStyle: {
+  viewOneStyle: {
     borderWidth: 3,
-    borderColor: "red",
+    backgroundColor: "red",
+    //flex: 1,
+    height: 50,
+    width:50
   },
-  textTwoStyle: {
+  viewTwoStyle: {
     borderWidth: 3,
-    borderColor: "red",
-    fontSize: 18,
-    ...StyleSheet.absoluteFillObject
+    backgroundColor: "green",
+    //flex: 1,
+    height: 50,
+    width:50,
+    top: 50,
   },
-  textThreeStyle: {
+  viewThreeStyle: {
     borderWidth: 3,
-    borderColor: "red",
+    backgroundColor: "purple",
+    //flex: 1,
+    height: 50,
+    width: 50
   },
 });
 

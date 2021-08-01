857142e4917e32748aaf4711ce593475a04e3ce7
Antonio Zea Jr <halosprtn117@gmail.com>
2021-07-31 22:44:41 -0400

    spacing  and margins styling

--- reactnative/food/src/components/ResultsList.js
+++ reactnative/food/src/components/ResultsList.js
@@ -1,26 +1,35 @@
-import React from  'react';
-import { View, Text, StyleSheet, FlatList } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+import ResultsDetail from "./ResultsDetail";
 
-const ResultsList = ({title, results}) => {
-    return <View>
-        <Text style={styles.titleStyle}>{title}</Text>
-        <FlatList
-            horizontal
-            data={results}
-            keyExtractor={(result) => result.id}
-            renderItem={({ item }) => {
-                return <Text>{item.name}</Text>;
-            }}
-        />
-        <Text>Results: {results.length}</Text>
+const ResultsList = ({ title, results }) => {
+  return (
+    <View style ={styles.container}>
+      <Text style={styles.titleStyle}>{title}</Text>
+      <FlatList
+        horizontal
+        data={results}
+        keyExtractor={(result) => result.id}
+        renderItem={({ item }) => {
+          return <ResultsDetail result={item}/>;
+        }}
+      />
+      <Text>Results: {results.length}</Text>
     </View>
-}
+  );
+};
 
 const styles = StyleSheet.create({
-    titleStyle:{
-      fontSize: 18,
-      fontWeight: "bold"
-    }
+  titleStyle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginLeft: 15,
+    marginBottom: 5
+  },
+
+  container: {
+      marginBottom: 10
+  }
 });
 
 export default ResultsList;

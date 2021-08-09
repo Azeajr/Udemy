1f354ba464e61864cb69754e2d08f494ec0de2c2
Antonio Zea Jr <halosprtn117@gmail.com>
2021-08-09 10:06:42 -0400

    Used an epmty element to organize sub elements

--- reactnative/food/src/screens/SearchScreen.js
+++ reactnative/food/src/screens/SearchScreen.js
@@ -15,7 +15,7 @@ const SearchScreen = () => {
   };
 
   return (
-    <View style={{flex: 1}}>
+    <>
       <SearchBar
         term={term}
         onTermChange={setTerm}
@@ -39,7 +39,7 @@ const SearchScreen = () => {
           title="Big Spender"
         />
       </ScrollView>
-    </View>
+    </>
   );
 };
 

04597f27e2364e14297f3a999b22de0440545711
Antonio Zea Jr <halosprtn117@gmail.com>
2021-08-26 14:10:45 -0400

    Completed video length app

--- electron_developer/videoinfo/index.js
+++ electron_developer/videoinfo/index.js
@@ -1,8 +1,30 @@
 const electron = require("electron");
+const ffmpeg = require("fluent-ffmpeg");
+const { app, BrowserWindow, ipcMain } = electron;
 
-const { app, BrowserWindow } = electron;
+let mainWindow;
 
 app.on("ready", () => {
-  const mainWindow = new BrowserWindow({});
+  mainWindow = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
   mainWindow.loadURL(`file://${__dirname}/index.html`);
+});
+/**
+ *
+ * ipcMain.on receives message from renderer to MainProcess
+ * mainWindow.webContents.send sends message from MainProcess to Renderer
+ *
+ * ipcRenderer.send sends message from Renderer to MainProcess
+ * ipcRenderer.on receives message from Main Process to Renderer
+ */
+
+// Receives path from Renderer
+ipcMain.on("video:submit", (event, path) => {
+  ffmpeg.ffprobe(path, (err, metadata) => {
+    mainWindow.webContents.send("video:metadata", metadata.format.duration);
+  });
 });

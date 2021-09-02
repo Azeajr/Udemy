d49a2a626d2548abd328eccea07c71e53812eaea
Antonio Zea Jr <halosprtn117@gmail.com>
2021-09-01 21:19:00 -0400

    Todos project start

---
+++ electron_developer/todos/index.js
@@ -0,0 +1,43 @@
+const electron = require("electron");
+
+const { app, BrowserWindow, Menu } = electron;
+
+let mainWindow;
+
+app.on("ready", () => {
+  mainWindow = new BrowserWindow({});
+  //mainWindow.loadURL(`file://${__dirname}/main.html`);
+  /**
+   * Preferred method
+   */
+  mainWindow.loadFile("main.html");
+
+  const mainMenu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(mainMenu);
+});
+
+const menuTemplate = [
+  // Old setup
+  {
+    label: "File",
+    submenu: [
+      { label: "New Todo" },
+      {
+        label: "Quit",
+        click() {
+          app.quit();
+        },
+      },
+    ],
+  },
+  // My solution
+  // checks if app is run on OSX, if true adds blank label
+  //   ...(process.platform === "darwin" ? [{ label: "" }] : []),
+  //   { label: "File", submenu: [{ label: "New Todo" }] },
+];
+// Suggested solution
+if (process.platform === "darwin") {
+  menuTemplate.unshift({
+    label: "",
+  });
+}

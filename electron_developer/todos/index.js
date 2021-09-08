bc6ec74752c63a3af1ff1a415430d9f848ef46b1
Antonio Zea Jr <halosprtn117@gmail.com>
2021-09-07 20:31:12 -0400

    IPC between windows

--- electron_developer/todos/index.js
+++ electron_developer/todos/index.js
@@ -1,6 +1,6 @@
 const electron = require("electron");
 
-const { app, BrowserWindow, Menu } = electron;
+const { app, BrowserWindow, Menu, ipcMain } = electron;
 
 let mainWindow;
 let addWindow;
@@ -26,6 +26,10 @@ function createAddWindow() {
   });
   addWindow.loadFile("add.html");
 }
+
+ipcMain.on("todo:add", (event, todo) => {
+  mainWindow.webContents.send("todo:add", todo);
+});
 
 const menuTemplate = [
   // Old setup

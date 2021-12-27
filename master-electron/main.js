e03bca235b5340e8cb789658a8b6e5f7b6845ab5
Antonio Zea Jr <halosprtn117@gmail.com>
2021-12-27 15:16:38 -0500

    adds items to be added to readit list

--- master-electron/main.js
+++ master-electron/main.js
@@ -1,13 +1,16 @@
 // Modules
 const { app, BrowserWindow, ipcMain } = require('electron')
 const windowStateKeeper = require('electron-window-state')
+const readItem = require('./readItem')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
 ipcMain.on('new-item', (e, itemUrl) => {
-  console.log(itemUrl)
+  readItem(itemUrl, item => {
+    e.sender.send('new-item-success', item)
+  })
 })
 
 // Create a new BrowserWindow when `app` is ready

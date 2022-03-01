e97be1ca2e2b13928e0cee7fdd0a2b79966780fd
Antonio Zea Jr <halosprtn117@gmail.com>
2022-02-28 21:52:17 -0500

    adds max.cpp

--- learnc++/Sec5/first.cpp
+++ learnc++/Sec5/first.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    string str;
-    cout << " this is cool" << endl;
-    cin >> str;
-    cout << "Welcome " << str << "Tell me something about yourself." << endl;
+int main() {
+  string str;
+  cout << " this is cool" << endl;
+  cin >> str;
+  cout << "Welcome " << str << "Tell me something about yourself." << endl;
 
-    return 0;
+  return 0;
 }
---
+++ learnc++/sec6/max.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+void max(int x, int y) {
+  if (x > y) {
+    std::cout << x;
+  } else {
+    std::cout << y;
+  }
+}
+
+int main(int argc, char const *argv[]) {
+  /* code */
+  return 0;
+}

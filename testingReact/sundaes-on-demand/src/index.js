8c2e8b812f5fb599ee4a332cdc6fd7bc497570da
Antonio Zea Jr <halosprtn117@gmail.com>
2022-04-08 19:51:40 -0400

    Update testing library and index to use React18

--- testingReact/sundaes-on-demand/src/index.js
+++ testingReact/sundaes-on-demand/src/index.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.render(
-  <React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
+root.render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function

5b46e1febd067cf961e1e432d28aceb1d39d6c1f
Antonio Zea Jr <halosprtn117@gmail.com>
2022-04-09 20:41:26 -0400

    add ToppingOption with testing

--- testingReact/sundaes-on-demand/src/pages/entry/Options.jsx
+++ testingReact/sundaes-on-demand/src/pages/entry/Options.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import Row from "react-bootstrap/Row";
 import ScoopOption from "./ScoopOption";
+import ToppingOption from "./ToppingOption";
 
 const Options = ({ optionType }) => {
   const [items, setItems] = useState([]);
@@ -15,8 +16,7 @@ const Options = ({ optionType }) => {
       });
   }, [optionType]);
 
-  // TODO: replace with ToppingOption when available
-  const ItemComponent = optionType === "scoops" ? ScoopOption : null;
+  const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
 
   const optionItems = items.map((item) => (
     <ItemComponent

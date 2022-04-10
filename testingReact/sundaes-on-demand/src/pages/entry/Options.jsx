11bf44c22f65dc260d4878a325a0111295bed175
Antonio Zea Jr <halosprtn117@gmail.com>
2022-04-10 15:54:49 -0400

    Add server error alerts and associated tests
    Test that proper alerts are shown when the server fails to retreive
    resources

--- testingReact/sundaes-on-demand/src/pages/entry/Options.jsx
+++ testingReact/sundaes-on-demand/src/pages/entry/Options.jsx
@@ -3,18 +3,22 @@ import { useEffect, useState } from "react";
 import Row from "react-bootstrap/Row";
 import ScoopOption from "./ScoopOption";
 import ToppingOption from "./ToppingOption";
+import AlertBanner from "../common/AlertBanner";
 
 const Options = ({ optionType }) => {
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     axios
       .get(`http://localhost:3030/${optionType}`)
       .then((response) => setItems(response.data))
-      .catch((error) => {
-        // TODO: handle error response
-      });
+      .catch((error) => setError(true));
   }, [optionType]);
+
+  if (error) {
+    return <AlertBanner />;
+  }
 
   const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
 

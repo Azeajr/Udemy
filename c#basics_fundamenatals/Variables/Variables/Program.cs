fd5c4c6b8ef28a4c7fd9d861a8c395b5ca79daae
Antonio Zea Jr <zeajantonio@gmail.com>
2022-06-30 23:33:48 -0400

    add Variables practice program

---
+++ c#basics_fundamenatals/Variables/Variables/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte number = 2;
+            int count = 10;
+            float totalPrice = 29.95f;
+            char character = 'A';
+            string firstName = "Antonio";
+            bool isWorking = false;
+
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+        }
+    }
+}

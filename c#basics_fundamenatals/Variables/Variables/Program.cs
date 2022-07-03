195f1e26a997ba508f32eadb196c7aa08fc0e6b4
Antonio Zea Jr <zeajantonio@gmail.com>
2022-07-03 15:27:03 -0400

    add new solution usind Ryder

--- c#basics_fundamenatals/Variables/Variables/Program.cs
+++ c#basics_fundamenatals/Variables/Variables/Program.cs
@@ -1,28 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Variables
+﻿namespace Variables
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            byte number = 2;
-            int count = 10;
-            float totalPrice = 29.95f;
-            char character = 'A';
-            string firstName = "Antonio";
-            bool isWorking = false;
-
-            Console.WriteLine(number);
-            Console.WriteLine(count);
-            Console.WriteLine(totalPrice);
-            Console.WriteLine(character);
-            Console.WriteLine(firstName);
-            Console.WriteLine(isWorking);
         }
     }
 }

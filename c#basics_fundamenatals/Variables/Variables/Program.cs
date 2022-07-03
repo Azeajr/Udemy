6e09b197c5d5035217a8986598e098a17a49ea83
Antonio Zea Jr <zeajantonio@gmail.com>
2022-07-03 15:40:15 -0400

    add variables

--- c#basics_fundamenatals/Variables/Variables/Program.cs
+++ c#basics_fundamenatals/Variables/Variables/Program.cs
@@ -1,9 +1,24 @@
-﻿namespace Variables
+﻿using System;
+
+namespace Variables
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            byte number = 2;
+            int count = 10;
+            float totalPrice = 29.95f;
+            char character = 'A';
+            string firstName = "Antonio";
+            bool isWorking = false;
+
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
         }
     }
 }

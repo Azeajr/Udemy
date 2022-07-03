19a09d9cc2069fc410677da02c5a0c82e03e68a3
Antonio Zea Jr <zeajantonio@gmail.com>
2022-07-03 15:59:39 -0400

    change variable types to var

--- c#basics_fundamenatals/Variables/Variables/Program.cs
+++ c#basics_fundamenatals/Variables/Variables/Program.cs
@@ -6,12 +6,12 @@ namespace Variables
     {
         public static void Main(string[] args)
         {
-            byte number = 2;
-            int count = 10;
-            float totalPrice = 29.95f;
-            char character = 'A';
-            string firstName = "Antonio";
-            bool isWorking = false;
+            var number = 2;
+            var count = 10;
+            var totalPrice = 29.95f;
+            var character = 'A';
+            var firstName = "Antonio";
+            var isWorking = false;
 
             Console.WriteLine(number);
             Console.WriteLine(count);

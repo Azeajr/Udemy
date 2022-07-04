3b84d8aef79bcf7c0b02d0305b099ca2e383de5c
Antonio Zea Jr <zeajantonio@gmail.com>
2022-07-04 14:29:34 -0400

    add enums demo

---
+++ c#basics_fundamenatals/Enums/Enums/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Enums
+{
+    public enum ShippingMethod
+    {
+        RegularAirMail = 1,
+        RegisteredAirMail = 2,
+        Express = 3
+    }
+
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var method = ShippingMethod.Express;
+            Console.WriteLine(method);
+            Console.WriteLine((int)method);
+
+            var methodId = 3;
+            Console.WriteLine((ShippingMethod)methodId);
+            
+            Console.WriteLine(method.ToString());
+
+            var methodName = "Express";
+            
+            var shippingMethod = (ShippingMethod) Enum.Parse((typeof(ShippingMethod)),methodName);
+            
+            Console.WriteLine(shippingMethod);
+        }
+    }
+}

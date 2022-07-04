66ca2e9518f799fece3c89a6c376328e742f25a2
Antonio Zea Jr <zeajantonio@gmail.com>
2022-07-04 11:26:53 -0400

    add classes demo

---
+++ c#basics_fundamenatals/Classes/Classes/Math/Calculator.cs
@@ -0,0 +1,10 @@
+namespace Classes.Math
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ c#basics_fundamenatals/Classes/Classes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Classes.Math;
+
+namespace Classes
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var john = new Person();
+            john.FirstName = "John";
+            john.LastName = "Smith";
+            
+            john.Introduce();
+
+            Calculator calculator = new Calculator();
+            var result = calculator.Add(1, 2);
+            Console.WriteLine(result);
+        }
+    }
+}

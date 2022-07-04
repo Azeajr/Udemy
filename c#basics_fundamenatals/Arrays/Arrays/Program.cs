be4f414e7af039e3997f70b65b4ac0ae803a9cc7
Antonio Zea Jr <zeajantonio@gmail.com>
2022-07-04 11:33:20 -0400

    add arrays demo

---
+++ c#basics_fundamenatals/Arrays/Arrays/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] numbers = new int[3];
+            numbers[0] = 1;
+
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
+        }
+    }
+}

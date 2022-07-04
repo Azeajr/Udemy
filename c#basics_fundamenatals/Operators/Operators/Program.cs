63a3b96c204730c8c7bc4985b1dc59e257966b7c
Antonio Zea Jr <zeajantonio@gmail.com>
2022-07-04 09:42:15 -0400

    add operators demo

---
+++ c#basics_fundamenatals/Operators/Operators/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Operators
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var a = 1;
+            var b = 2;
+            var c = 3;
+
+            Console.WriteLine((a + b) * c);
+            Console.WriteLine(a == b);
+            Console.WriteLine(a != b);
+            Console.WriteLine((c > b || c == a));
+        }
+    }
+}

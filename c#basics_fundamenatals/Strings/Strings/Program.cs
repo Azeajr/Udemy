e635f0329a759e0332a17d9f5944d07cd4ffeb78
Antonio Zea Jr <zeajantonio@gmail.com>
2022-07-04 12:00:16 -0400

    add strings demo

---
+++ c#basics_fundamenatals/Strings/Strings/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Strings
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var firstName = "Antonio";
+            var lastName = "Zea";
+
+            var fullName = firstName + " " + lastName;
+            var myFullName = string.Format("My name is {0} {1}", firstName, lastName);
+
+            var names = new string[3] { "John", "Jack", "Mary" };
+            var formattedNames = string.Join(",", names);
+            Console.WriteLine(formattedNames);
+
+            var text = "Hi John\nLook into the following paths\nc:\\folder1\\folder2\nc:\\folder3\\folder4";
+            Console.WriteLine(text);
+
+            text = @"Hi John
+Look into the following paths
+c:\folder1\folder2
+c:\folder3\folder4";
+            
+            Console.WriteLine(text);
+        }
+    }
+}

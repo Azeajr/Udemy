08d8df591cf153fb81a3e69f7a50db248c8a130c
Antonio Zea Jr <zeajantonio@gmail.com>
2022-07-04 11:37:44 -0400

    refactor and demo boolean and string arrays

--- c#basics_fundamenatals/Arrays/Arrays/Program.cs
+++ c#basics_fundamenatals/Arrays/Arrays/Program.cs
@@ -6,12 +6,24 @@ namespace Arrays
     {
         public static void Main(string[] args)
         {
-            int[] numbers = new int[3];
+            var numbers = new int[3];
             numbers[0] = 1;
 
             Console.WriteLine(numbers[0]);
             Console.WriteLine(numbers[1]);
             Console.WriteLine(numbers[2]);
+
+            var flags = new bool[3];
+            flags[0] = true;
+            
+            Console.WriteLine(flags[0]);
+            Console.WriteLine(flags[1]);
+            Console.WriteLine(flags[2]);
+
+            var names = new string[3] { "Jack", "John", "Mary" };
+            Console.WriteLine(names[0]);
+            Console.WriteLine(names[1]);
+            Console.WriteLine(names[2]);
         }
     }
 }

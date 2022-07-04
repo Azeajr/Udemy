5418344e9e85ba2fafaab8d54952f316f9abab84
Antonio Zea Jr <zeajantonio@gmail.com>
2022-07-04 08:45:22 -0400

    add type conversion demo

---
+++ c#basics_fundamenatals/TypeConversion/TypeConversion/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace TypeConversion
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            byte b = 1;
+            int i = b;
+
+            Console.WriteLine(i);
+
+            i = 1000;
+
+            b = (byte)i;
+
+            Console.WriteLine(b);
+
+            Console.WriteLine(i % 256);
+
+            var number = "1234";
+
+            i = Convert.ToInt32(number);
+            Console.WriteLine(i);
+            b = (byte)Convert.ToInt32(number);
+            Console.WriteLine(b);
+            Console.WriteLine(i % 256);
+
+            try
+            {
+                b = Convert.ToByte(number);
+                Console.WriteLine(b);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                // throw;
+            }
+        }
+    }
+}

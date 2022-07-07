85147cdbd4fd7e8592fc65a6b766de21cd5a29da
Antonio Zea Jr <zeajantonio@gmail.com>
2022-07-07 16:34:41 -0400

    add graphql server

---
+++ graphql_example/hello-world/server/server.js
@@ -0,0 +1,24 @@
+import {ApolloServer, gql } from "apollo-server";
+
+const typeDefs = gql`
+  schema {
+    query: Query
+  }
+
+  type Query {
+    greeting: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    greeting: () => 'Hello world!',
+  }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+// const serverInfo = await server.listen({ port: 9000 });
+const {url} = await server.listen({ port: 9000 });
+
+
+console.log(`Server running at ${url}`);

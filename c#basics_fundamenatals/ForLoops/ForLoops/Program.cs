bf7b8721479a7eade8631bf8bf542e92cbb68ab5
Antonio Zea Jr <zeajantonio@gmail.com>
2022-07-07 22:17:33 -0400

    add for loops demo

---
+++ c#basics_fundamenatals/ForLoops/ForLoops/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ForLoops
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            for (var i = 10; i >= 1; i--)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            
+        }
+    }
+}

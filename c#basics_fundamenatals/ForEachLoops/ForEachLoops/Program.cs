424f871bd46f71d3e1bb5a6a5c4ebc1880ef1ad5
Antonio Zea Jr <zeajantonio@gmail.com>
2022-07-09 23:39:13 -0400

    add for each loops demo

---
+++ c#basics_fundamenatals/ForEachLoops/ForEachLoops/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ForEachLoops
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var name = "John Smith";
+
+            // for (var i = 0; i < name.Length; i++)
+            // {
+            //     Console.WriteLine(name[i]);
+            // }
+
+            foreach (var character in name)
+            {
+                Console.WriteLine(character);
+            }
+
+            var numbers = new int[] { 1, 2, 3, 4 };
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}

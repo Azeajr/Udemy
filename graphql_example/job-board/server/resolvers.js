d828df39fd1308f53a8cb874ed0c2988cc757b7d
Antonio Zea Jr <zeajantonio@gmail.com>
2022-07-21 00:34:40 -0400

    adds graphql-request client

--- graphql_example/job-board/server/resolvers.js
+++ graphql_example/job-board/server/resolvers.js
@@ -1,5 +1,11 @@
+import { Company, Job } from "./db.js";
+
 export const resolvers = {
   Query: {
-    jobs: () => [],
+    jobs: async () => Job.findAll(),
+  },
+
+  Job: {
+    company: (job) => Company.findById(job.companyId),
   },
 };

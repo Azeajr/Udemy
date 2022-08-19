f150de6f04217e648db83dd10e2ca476671ad185
Antonio Zea Jr <zeajantonio@gmail.com>
2022-08-19 09:45:20 -0400

    adds finished routes to backend

--- express-jwt-authentication-starter/config/passport.js
+++ express-jwt-authentication-starter/config/passport.js
@@ -1,12 +1,32 @@
 const fs = require('fs');
 const path = require('path');
 const User = require('mongoose').model('User');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
 
 const pathToKey = path.join(__dirname, '..', 'id_rsa_pub.pem');
 const PUB_KEY = fs.readFileSync(pathToKey, 'utf8');
 
-// TODO
-const options = {};
+// [x]
+const options = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: PUB_KEY,
+  algorithms: ['RS256']
+};
 
-// TODO
-module.exports = (passport) => {}
+const strategy = new JwtStrategy(options, (payload, done) => {
+  User.findOne({_id: payload.sub})
+      .then((user) => {
+        if (user) {
+          return done(null, user);
+        } else {
+          return done(null, false);
+        }
+      })
+      .catch(err => done(err, null));
+});
+
+// [x]
+module.exports = (passport) => {
+  passport.use(strategy);
+}
--- express-jwt-authentication-starter/routes/users.js
+++ express-jwt-authentication-starter/routes/users.js
@@ -1,17 +1,61 @@
 const mongoose = require('mongoose');
-const router = require('express').Router();   
+const router = require('express').Router();
 const User = mongoose.model('User');
 const passport = require('passport');
 const utils = require('../lib/utils');
 
 // TODO
-router.get('/protected', (req, res, next) => {
+router.get(
+    '/protected', passport.authenticate('jwt', {session: false}),
+    (req, res, next) => {
+      res.status(200).json({success: true, msg: 'You are authorized!'});
+    });
+
+// TODO
+router.post('/login', function(req, res, next) {
+  User.findOne({username: req.body.username})
+      .then((user) => {
+        if (!user) {
+          res.status(401).json({success: false, msg: 'User not found'});
+        }
+
+        const isValid =
+            utils.validPassword(req.body.password, user.hash, user.salt);
+
+        if (isValid) {
+          const tokenObject = utils.issueJWT(user);
+          res.status(200).json({
+            success: true,
+            user,
+            token: tokenObject.token,
+            expiresIn: tokenObject.expires
+          });
+        } else {
+          res.status(401).json({success: false, msg: 'Wrong password'})
+        }
+      })
+      .catch((err) => {
+        next(err);
+      });
 });
 
 // TODO
-router.post('/login', function(req, res, next){});
+router.post('/register', function(req, res, next) {
+  const saltHash = utils.genPassword(req.body.password);
 
-// TODO
-router.post('/register', function(req, res, next){});
+  const salt = saltHash.salt;
+  const hash = saltHash.hash;
+
+  const newUser = new User({username: req.body.username, hash, salt});
+
+  newUser.save()
+      .then((user) => {
+        const jwt = utils.issueJWT(user);
+
+        res.json(
+            {success: true, user, token: jwt.token, expiresIn: jwt.expires});
+      })
+      .catch(err => next(err));
+});
 
 module.exports = router;

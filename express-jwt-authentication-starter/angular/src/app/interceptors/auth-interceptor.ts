e9f46e42f631eb5276529a940492e28f2cb6d85d
Antonio Zea Jr <zeajantonio@gmail.com>
2022-08-19 21:15:20 -0400

    fixes misspelled localStorage item

--- express-jwt-authentication-starter/angular/src/app/interceptors/auth-interceptor.ts
+++ express-jwt-authentication-starter/angular/src/app/interceptors/auth-interceptor.ts
@@ -1,24 +1,21 @@
-import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest,} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler):
+      Observable<HttpEvent<any>> {
+    const idToken = localStorage.getItem('token');
 
-    intercept(req: HttpRequest<any>,
-              next: HttpHandler): Observable<HttpEvent<any>> {
+    if (idToken) {
+      const cloned = req.clone({
+        headers: req.headers.set('Authorization', idToken),
+      });
 
-        const idToken = localStorage.getItem("id_token");
-
-        if (idToken) {
-            const cloned = req.clone({
-                headers: req.headers.set("Authorization", idToken)
-            });
-
-            return next.handle(cloned);
-        }
-        else {
-            return next.handle(req);
-        }
+      return next.handle(cloned);
+    } else {
+      return next.handle(req);
     }
+  }
 }

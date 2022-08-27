d8d0959af54d542740575bd430e22bb797e4a7c0
Antonio Zea Jr <zeajantonio@gmail.com>
2022-08-27 14:58:56 -0400

    adds completed nodejs Routing tutorial

--- nodejs_tutorial/08TUT/server.js
+++ nodejs_tutorial/08TUT/server.js
@@ -24,52 +24,17 @@ app.use(cors(corsOptions));
 
 app.use(express.urlencoded({extended: false}));
 
+// Builtin middleware for json
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, '/public')));
+// Serve static files
+app.use('/', express.static(path.join(__dirname, '/public')));
+app.use('/subdir', express.static(path.join(__dirname, '/public')));
 
-app.get('^/$|index(.html)?', (req, res) => {
-  //   res.sendFile('./views/index.html', {root:__dirname});
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
-});
-
-app.get('/new-page(.html)?', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
-});
-
-app.get('/old-page(.html)?', (req, res) => {
-  res.redirect(301, '/new-page.html');
-});
-
-// Route Handlers
-app.get(
-    '/hello(.html)?',
-    (req, res, next) => {
-      console.log('attempted to load hello.html');
-      next();
-    },
-    (req, res) => {
-      res.send('Hello WORLD');
-    });
-
-
-
-const one = (req, res, next) => {
-  console.log('one');
-  next();
-};
-
-const two = (req, res, next) => {
-  console.log('two');
-  next();
-};
-
-const three = (req, res) => {
-  console.log('three');
-  res.send('Finished!')
-};
-
-app.get('/chain(.html)?', [one, two, three]);
+// Routes
+app.use('/', require('./routes/root'));
+app.use('/subdir', require('./routes/subdir'));
+app.use('/employees', require('./routes/api/employees'));
 
 app.all('*', (req, res) => {
   res.status(404);

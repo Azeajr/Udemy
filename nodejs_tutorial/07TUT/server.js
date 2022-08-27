2eb19dbfbec4af42193cd4a98d23a956022e6f9b
Antonio Zea Jr <zeajantonio@gmail.com>
2022-08-27 12:20:19 -0400

    adds completed NodeJs Middleware tutorial

--- nodejs_tutorial/07TUT/server.js
+++ nodejs_tutorial/07TUT/server.js
@@ -1,7 +1,32 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const path = require('path');
+const {logger} = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
+const {appendFileSync} = require('fs');
 const PORT = process.env.PORT || 3500;
+
+app.use(logger);
+
+const whitelist = ['https://www.yoursite.com', 'http://localhost:3500'];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
+app.use(express.urlencoded({extended: false}));
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('^/$|index(.html)?', (req, res) => {
   //   res.sendFile('./views/index.html', {root:__dirname});
@@ -46,9 +71,18 @@ const three = (req, res) => {
 
 app.get('/chain(.html)?', [one, two, three]);
 
-app.get('/*', (req, res) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+app.all('*', (req, res) => {
+  res.status(404);
+  if (req.accepts('html')) {
+    res.sendFile(path.join(__dirname, 'views', '404.html'));
+  } else if (req.accepts('json')) {
+    res.json({error: '404 Not Found'});
+  } else {
+    res.type('txt').send('404 Not Found');
+  }
 });
+
+app.use(errorHandler)
 
 app.listen(
     PORT, () => console.log(`Server running on port http://localhost:${PORT}`));

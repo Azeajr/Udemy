b77d25a9074e7d6f453a83c9f295af31a6b346c6
Antonio Zea Jr <zeajantonio@gmail.com>
2022-09-02 15:31:14 -0400

    adds completed JWT authentication and authorization

--- nodejs_tutorial/11TUT/server.js
+++ nodejs_tutorial/11TUT/server.js
@@ -5,10 +5,15 @@ const app = express();
 const path = require('path');
 const {logger} = require('./middleware/logEvents');
 const errorHandler = require('./middleware/errorHandler');
-const {appendFileSync} = require('fs');
+const verifyJWT = require('./middleware/verifyJWT');
+const cookieParser = require('cookie-parser');
+const credentials = require('./middleware/credentials');
+
 const PORT = process.env.PORT || 3500;
 
 app.use(logger);
+
+app.use(credentials);
 
 app.use(cors(corsOptions));
 
@@ -17,6 +22,9 @@ app.use(express.urlencoded({extended: false}));
 // Builtin middleware for json
 app.use(express.json());
 
+// Middleware for cookies
+app.use(cookieParser());
+
 // Serve static files
 app.use('/', express.static(path.join(__dirname, '/public')));
 
@@ -24,6 +32,11 @@ app.use('/', express.static(path.join(__dirname, '/public')));
 app.use('/', require('./routes/root'));
 app.use('/register', require('./routes/register'));
 app.use('/auth', require('./routes/auth'));
+app.use('/refresh', require('./routes/refresh'));
+app.use('/logout', require('./routes/logout'));
+
+app.use(verifyJWT);
+
 app.use('/employees', require('./routes/api/employees'));
 
 app.all('*', (req, res) => {

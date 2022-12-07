f302ec14add46018a07342aa284a3b97e61d1c37
Antonio Zea Jr <zeajantonio@gmail.com>
2022-12-07 18:11:47 -0500

    WIP authController

--- nodejs_tutorial/13TUT/controllers/authController.js
+++ nodejs_tutorial/13TUT/controllers/authController.js
@@ -1,41 +1,48 @@
 const User = require("../model/User");
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const handleLogin = async (req, res) => {
-  const {user, pwd} = req.body;
+  const { user, pwd } = req.body;
   if (!user || !pwd)
-    return res.status(400).json(
-        {'message': 'Username and password are required.'});
+    return res
+      .status(400)
+      .json({ message: "Username and password are required." });
 
   const foundUser = await User.findOne({ username: user }).exec();
   if (!foundUser) return res.sendStatus(401);
 
   const match = await bcrypt.compare(pwd, foundUser.password);
   if (match) {
-    const roles = Object.values(foundUser.roles);
+    // TODO: This filter does not look right.
+    const roles = Object.values(foundUser.roles).filter(Boolean);
 
     const accessToken = jwt.sign(
-        {'UserInfo': {'username': foundUser.username, 'roles': roles}},
-        process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1m'});
+      { UserInfo: { username: foundUser.username, roles: roles } },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "1m" }
+    );
     const refreshToken = jwt.sign(
-        {'username': foundUser.username}, process.env.REFRESH_TOKEN_SECRET,
-        {expiresIn: '1d'});
+      { username: foundUser.username },
+      process.env.REFRESH_TOKEN_SECRET,
+      { expiresIn: "1d" }
+    );
 
     foundUser.refreshToken = refreshToken;
     const result = await foundUser.save();
     console.log(result);
+    console.log(roles);
 
-    res.cookie('jwt', refreshToken, {
+    res.cookie("jwt", refreshToken, {
       httpOnly: true,
-      sameSite: 'None',
+      sameSite: "None",
       secure: true,
-      maxAge: 24 * 60 * 60 * 1000
+      maxAge: 24 * 60 * 60 * 1000,
     });
-    res.json({accessToken});
+    res.json({ roles, accessToken });
   } else {
     res.sendStatus(401);
   }
 };
 
-module.exports = {handleLogin};
+module.exports = { handleLogin };

cf6582acb0de2c5b671a791d321bef41111a531e
Antonio Zea Jr <zeajantonio@gmail.com>
2022-12-07 18:12:20 -0500

    Adds react_login practice project

--- react_loginRegAuth/src/App.js
+++ react_loginRegAuth/src/App.js
@@ -1,11 +1,23 @@
-import Register from './Register';
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import Layout from "./components/Layout";
+import Editor from "./components/Editor";
+import Admin from "./components/Admin";
+import Missing from "./components/Missing";
+import Unauthorized from "./components/Unauthorized";
+import Lounge from "./components/Lounge";
+import LinkPage from "./components/LinkPage";
+import RequireAuth from "./components/RequireAuth";
+import { Routes, Route } from "rwact-router-dom";
 
 function App() {
-
   return (
-    <main className="App">
-      <Register />
-    </main>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="login" element={<Login />} />
+      </Route>
+    </Routes>
   );
 }
 

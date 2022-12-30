362b18533598049dc36cd7b6c66af80266247626
Antonio Zea Jr <zeajantonio@gmail.com>
2022-12-30 00:01:16 -0500

    Make last param of changeset optional

--- elixir_phoenix/discuss/lib/discuss/topic.ex
+++ elixir_phoenix/discuss/lib/discuss/topic.ex
@@ -3,13 +3,13 @@ defmodule Discuss.Topic do
   import Ecto.Changeset
 
   schema "topics" do
-    field :title, :string
+    field(:title, :string)
 
     timestamps()
   end
 
   @doc false
-  def changeset(topic, attrs) do
+  def changeset(topic, attrs \\ %{}) do
     topic
     |> cast(attrs, [:title])
     |> validate_required([:title])

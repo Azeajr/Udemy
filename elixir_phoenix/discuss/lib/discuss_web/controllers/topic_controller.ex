08267c9a21a1b46562e104a09a6a816fc3b9fcd3
Antonio Zea Jr <zeajantonio@gmail.com>
2022-12-30 00:03:45 -0500

    Complete edit route

--- elixir_phoenix/discuss/lib/discuss_web/controllers/topic_controller.ex
+++ elixir_phoenix/discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -13,7 +13,7 @@ defmodule DiscussWeb.TopicController do
     changeset = Topic.changeset(%Topic{}, topic)
 
     case Discuss.Repo.insert(changeset) do
-      {:ok, post} ->
+      {:ok, _topic} ->
         conn
         |> put_flash(:info, "Topic Created")
         |> redirect(to: Routes.topic_path(conn, :index))
@@ -26,5 +26,27 @@ defmodule DiscussWeb.TopicController do
   def index(conn, _params) do
     topics = Discuss.Repo.all(Topic)
     render(conn, "index.html", topics: topics)
+  end
+
+  def edit(conn, %{"id" => topic_id}) do
+    topic = Discuss.Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(topic)
+
+    render(conn, "edit.html", changeset: changeset, topic: topic)
+  end
+
+  def update(conn, %{"id" => topic_id, "topic" => topic}) do
+    old_topic = Discuss.Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(old_topic, topic)
+
+    case Discuss.Repo.update(changeset) do
+      {:ok, _topic} ->
+        conn
+        |> put_flash(:info, "Topic Updated")
+        |> redirect(to: Routes.topic_path(conn, :index))
+
+      {:error, changeset} ->
+        render(conn, "edit.html", changeset: changeset, topic: old_topic)
+    end
   end
 end

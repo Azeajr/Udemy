a34a3d06ec295d67bfa30e89fbc2f5e708bc8116
Antonio Zea Jr <zeajantonio@gmail.com>
2023-01-14 23:15:41 -0500

    feat(topic_controller): :sparkles: add show route to present a view for individual topics

--- elixir_phoenix/discuss/lib/discuss_web/controllers/topic_controller.ex
+++ elixir_phoenix/discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -6,6 +6,11 @@ defmodule DiscussWeb.TopicController do
   plug(DiscussWeb.Plugs.RequireAuth when action in [:new, :create, :edit, :update, :delete])
   plug(:check_topic_owner when action in [:update, :edit, :delete])
 
+  def show(conn, %{"id" => topic_id}) do
+    topic = Discuss.Repo.get!(Topic, topic_id)
+    render conn, "show.html", topic: topic
+  end
+
   def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
 

f85c736b7040b2a73566c52492cd8f90dcd87f6c
Antonio Zea Jr <zeajantonio@gmail.com>
2023-01-16 21:49:54 -0500

    feat(comments_channel): :sparkles: add comment backend channel connection to database
    comments_channel now can receive a comment from a given topic and record it in the database

--- elixir_phoenix/discuss/lib/discuss_web/channels/comments_channel.ex
+++ elixir_phoenix/discuss/lib/discuss_web/channels/comments_channel.ex
@@ -1,38 +1,30 @@
 defmodule DiscussWeb.CommentsChannel do
   use DiscussWeb, :channel
 
+  alias Discuss.{User, Comment, Repo}
+
   @impl true
-  # def join("comments:lobby", payload, socket) do
-  def join(name, payload, socket) do
-    # if authorized?(payload) do
-    #   {:ok, socket}
-    # else
-    #   {:error, %{reason: "unauthorized"}}
-    # end
-    {:ok, %{hey: "there"}, socket}
+  def join("comments:" <> topic_id, payload, socket) do
+    topic_id = String.to_integer(topic_id)
+    topic = Repo.get(Topic, topic_id)
+
+    {:ok, %{}, assign(socket, :topic, topic)}
   end
 
-  # Channels can be used in a request/response fashion
-  # by sending replies to requests from the client
-  @impl true
-  def handle_in(name, payload, socket) do
-  # def handle_in("ping", payload, socket) do
-    IO.puts("+++++++")
-    IO.puts(name)
-    IO.inspect(payload)
-    {:reply, {:ok, payload}, socket}
-  end
+  def handle_in(name, %{"content" => content}, socket) do
+    topic = socket.assigns.topic
 
-  # It is also common to receive messages from the client and
-  # broadcast to everyone in the current topic (comments:lobby).
-  @impl true
-  def handle_in("shout", payload, socket) do
-    broadcast(socket, "shout", payload)
-    {:noreply, socket}
-  end
+    changeset =
+      topic
+      |> Ecto.build_assoc(:comments)
+      |> Comment.changeset(%{content: content})
 
-  # Add authorization logic here as required.
-  defp authorized?(_payload) do
-    true
+    case Repo.insert(changeset) do
+      {:ok, comment} ->
+        {:reply, :ok, socket}
+
+      {error, _reason} ->
+        {:reply, {:error, %{errors: changeset}}, socket}
+    end
   end
 end

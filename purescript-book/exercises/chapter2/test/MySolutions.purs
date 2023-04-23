aa7ea099d33e12a453f57b5e3de6d5d8e1a0ede7
Antonio Zea Jr <zeajantonio@gmail.com>
2023-04-23 16:54:24 -0400

    feat(chapter2): :sparkles: add solutions for chapter2

--- purescript-book/exercises/chapter2/test/MySolutions.purs
+++ purescript-book/exercises/chapter2/test/MySolutions.purs
@@ -1,3 +1,12 @@
 module Test.MySolutions where
 
 import Prelude
+
+import Data.Number (sqrt, pi)
+import Data.Int (rem)
+
+diagonal w h = sqrt(w * w + h * h)
+
+circleArea r = pi * r * r
+
+leftoverCents cents = rem cents 100

3f6e7cffba628360a2dd1197b962a6943f8d5f96
Antonio Zea Jr <zeajantonio@gmail.com>
2023-05-08 21:19:54 -0400

    feat(purescript-book:chapter4): :sparkles: solutions for chapter 4 ex 1,2

--- purescript-book/exercises/chapter4/test/Main.purs
+++ purescript-book/exercises/chapter4/test/Main.purs
@@ -18,7 +18,6 @@ main :: Effect Unit
 main =
   runTest do
     runChapterExamples
-    {-  Move this block comment starting point to enable more tests
     suite "Exercise Group - Recursion" do
       suite "Exercise - isEven" do
         test "0 is even" do
@@ -55,6 +54,7 @@ main =
         test "[0, 1, 19, 20] has 2" do
           Assert.equal 2
             $ countEven [ 0, 1, 19, 20 ]
+{-  Move this block comment starting point to enable more tests
     suite "Exercise Group - Maps, Infix Operators, and Filtering" do
       suite "Exercise - squared" do
         test "Do nothing with empty array" do
--- purescript-book/exercises/chapter4/test/MySolutions.purs
+++ purescript-book/exercises/chapter4/test/MySolutions.purs
@@ -2,4 +2,25 @@ module Test.MySolutions where
 
 import Prelude
 
+import Data.Array (null, tail, head)
+import Data.Maybe (fromMaybe)
+
 -- Note to reader: Add your solutions to this file
+
+isEven :: Int -> Boolean
+isEven num =
+  if num - 2 == 0 then
+    true
+  else if num - 2 == 1 then
+    false
+  else
+    isEven (num - 2)
+
+countEven ∷ Array Int → Int
+countEven arr =
+  if null arr then
+    0
+  else if (isEven(fromMaybe 1 (head arr))) then
+    1 + countEven(fromMaybe [] (tail arr))
+  else
+    countEven(fromMaybe [] (tail arr))
